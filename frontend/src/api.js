55ebcd0f497da365ceaa78f3b0b6463e77071ff8
mlevsha <mlevsha@team.amocrm.com>
2024-04-01 12:03:58 +0300

    add refresh into api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,17 +2,45 @@ import axios from "axios";
 
 const api = axios.create()
 const token = localStorage.getItem('access_token')
+//start
 api.interceptors.request.use(config => {
-    if(token)
-    {
+    if (token) {
         config.headers.authorization = `Bearer ${token}`
 
-    } else{
+    } else {
         console.log('Token not found');
     }
     return config;
 }, error => {
     console.log(error.response)
 });
+//end
+api.interceptors.response.use(config => {
+        if (token) {
+            config.headers.authorization = `Bearer ${token}`
+
+        } else {
+            console.log('Token not found');
+        }
+        return config;
+    },
+    error => {
+        if (error.response.data.message === 'Token has expired') {
+            try {
+                axios.post('http://127.0.0.1:8000/api/auth/refresh', {}, {
+                    headers: {
+                        'authorization': `Bearer ${token}`
+                    }
+                }).then(res => {
+                    localStorage.setItem('access_token', res.data.access_token)
+                    error.config.headers.authorization = `Bearer ${token}`
+                    return api.request(error.config)
+                });
+            } catch (error) {
+                console.error('Error refresh', error.response);
+            }
+        }
+
+    })
 
 export default api
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,44 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name:'login.layout',
-      component: () => import('../views/Components/LoginLayout.vue'),
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '/login',
-          name:'login',
-          component: () => import('../views/User/Login.vue'),
+            path: '/',
+            name: 'login.layout',
+            component: () => import('../views/Components/LoginLayout.vue'),
+            children: [
+                {
+                    path: '/login',
+                    name: 'login',
+                    component: () => import('../views/User/Login.vue'),
+                },
+                {
+                    path: '/registration',
+                    name: 'login.registration',
+                    component: () => import('../views/User/Registration.vue'),
+                }
+            ]
         },
         {
-          path: '/registration',
-          name:'login.registration',
-          component: () => import('../views/User/Registration.vue'),
-        }
-      ]
-    },
-    {
-      path: '/button',
-      component: () => import('../views/Components/MainLayout.vue'),
-      children: [
-        {
-          path: '/button',
-          name: 'button',
-          component: () => import('../views/Index.vue')
+            path: '/button',
+            component: () => import('../views/Components/MainLayout.vue'),
+            children: [
+                {
+                    path: '/button',
+                    name: 'button',
+                    component: () => import('../views/Index.vue')
+                },
+                {
+                    path: '/settings',
+                    name: 'settings',
+                    component: () => import('../views/Pages/Settings.vue')
+                },
+            ]
         },
-        {
-          path: '/settings',
-          name: 'settings',
-          component: () => import('../views/Pages/Settings.vue')
-        },
-      ]
-    },
 
 
-  ]
+    ]
+})
+router.beforeEach((to, from, next) => {
+    console.log(to.name);
+    next();
 })
 
 export default router
