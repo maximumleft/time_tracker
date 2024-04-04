84dccab91e9856ada3d34ea58d66640963ee3ffa
mlevsha <mlevsha@team.amocrm.com>
2024-04-04 16:14:10 +0300

    paginate

--- frontend/src/views/Components/TimeTable.vue
+++ frontend/src/views/Components/TimeTable.vue
@@ -1,9 +1,19 @@
 <template>
-  <a-table
-      :columns="columns"
-      :data-source="formState.data"
-  >
-  </a-table>
+  <div>
+    <a-table
+        :columns="columns"
+        :data-source="formState.displayedData"
+        :pagination="false"
+    >
+    </a-table>
+    <a-pagination
+        style="margin-top: 20px; margin-left: 20px;"
+        :current="currentPage"
+        :total="formState.data.length"
+        :default-page-size="6"
+        @change="handleChangePage"
+    />
+  </div>
 </template>
 <script setup>
 import api from "../../api.js"
@@ -12,7 +22,10 @@ import {useRouter} from "vue-router";
 const router = useRouter()
 const formState = reactive({
   data: [],
+  displayedData: [],
 });
+
+let currentPage = 1;
 const columns = [
   {
     title: 'Name',
@@ -27,14 +40,23 @@ const columns = [
 onMounted(() => {
   getUsers();
 });
+
 async function getUsers(){
   try {
     const response = await api.get('http://127.0.0.1:8000/api/auth/users');
     console.log('ok');
-    formState.data = response.data.data
+    formState.data = response.data.data;
+    formState.displayedData = formState.data.slice(0, 6); // Первая страница с 6 элементами
   } catch (error) {
     console.error('Error get Users:', error.response.data.message);
   }
+}
+
+function handleChangePage(page) {
+  currentPage = page;
+  const startIndex = (page - 1) * 6;
+  const endIndex = startIndex + 6;
+  formState.displayedData = formState.data.slice(startIndex, endIndex);
 }
 
 
